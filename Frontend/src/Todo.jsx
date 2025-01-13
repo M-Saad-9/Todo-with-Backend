5f60521442f87abb1910fa8af1e0a83a4e109baa
Muhammad saad <saadrahimit789@gmail.com>
2025-01-13 09:58:06 -0800

    todo edit and save

--- Frontend/src/Todo.jsx
+++ Frontend/src/Todo.jsx
@@ -5,12 +5,18 @@ export default function Todo() {
   const API = "http://localhost:3000"
 
   const [todos,setTodos] = useState([])
+  console.log("new todoss data", todos);
+  const [textEdit,setTextEdit] = useState()
 
   const getTodos = async () => {
     const response = await axios(`${API}/api/todos`)
     const todoData = response?.data?.Data
-    console.log("todo",todoData);
-    setTodos(todoData)
+    // console.log("todo",todoData);
+
+    const newTodoData = todoData.map((todos) => {
+      return { ...todos, isEditing: false}
+    })
+    setTodos(newTodoData)
     // console.log(response);
     
   }
@@ -31,20 +37,38 @@ export default function Todo() {
         }
       )
       getTodos()
+      event.target.reset();
       
     } catch (err) {
 
     }
   }
 
+  const editTodo = async (event, todoId) => {
+    try {
+      event.preventDefault();
+
+      const todoValue = event.target.children[0].value;
+
+      await axios.patch(`${API}/api/todo/${todoId}`, {
+        todoDetail: todoValue,
+      });
+      getTodos();
+
+      event.target.reset();
+    } catch (err) {
+
+    }
+  };
+
   const deleteTodo = async (id) => {
     try {
-        console.log( id);
+        // console.log( id);
         const res = await axios.delete(`${API}/api/todo/${id}`);
         // console.log("Deleted successfully:", res);
         getTodos();
     } catch (error) {
-        console.error("Error deleting todo:", error.response?.data || error.message);
+        // console.error("Error deleting todo:", error.response?.data || error.message);
     }
 };
 
@@ -70,16 +94,53 @@ export default function Todo() {
       {/* Task List */}
       <ul className="space-y-4">
         {/* Example Task 1 */}
-        {todos?.map((item)=>(
+        {todos?.map((item,index)=>(
           <li 
           key={item.id}
           className="flex justify-between items-center p-4 bg-gray-50 border border-gray-200 rounded-md">
-          <span className="text-gray-700">{item.todoDetail}</span>
+          {!item.isEditing ? <span className="text-gray-700">{item.todoDetail}</span>
+          : 
+          <form  onSubmit={(e) => editTodo(e, item.id)}>
+             <input type="text" defaultValue={item.todoDetail}
+          className='border border-gray-900 w-[60%]'/>
+           <button
+            onClick={() => {
+              const newTodos = todos.map((todo, i) => {
+                todo.isEditing = false;
+                return todo;
+              });
+              setTodos([...newTodos]);
+            }}
+            type="button">Cancel</button>
+       <button type="submit">Save</button>
+          </form>
+          }
+          
           <div className="space-x-3">
-            <button className="text-blue-500 hover:text-blue-700">Edit</button>
-            <button
+            {/* yaha edit or save button ha */}
+          {!item.isEditing ? <button 
+            onClick={()=> {
+              const newTodos = todos.map((todo,i) => {
+                if (i === index) {
+                  todo.isEditing = true;
+                } else {
+                  todo.isEditing = false;
+                }
+                return todo;
+              });
+             
+              // todos[index].isEditing = true
+              setTodos([...newTodos]);
+            }}
+            className="text-blue-500 hover:text-blue-700">Edit</button>
+          : null}
+            
+
+            {/* yaha delete or cancel button ha */}
+            {!item.isEditing ? <button
             onClick={()=> deleteTodo(item.id)}
             className="text-red-500 hover:text-red-700">Delete</button>
+            :null}
           </div>
         </li>
         ))}
